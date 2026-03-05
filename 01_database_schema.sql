da0a9739998ae8c16e6fb20d4b1ccd28dc7a90bd
ARUN-web255 <arunpragash.k23@gmail.com>
2026-03-05 08:57:47 +0530

    Add database schema (tables and constraints)

--- 01_database_schema.sql
+++ 01_database_schema.sql
@@ -60,7 +60,6 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    amount DECIMAL(10,2),
     status VARCHAR(20),
 
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
